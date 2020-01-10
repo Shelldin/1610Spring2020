ed794ec1bbd5f53f76331bb8d63b81f4c9d6da6e
Sheldon Bean <sheldon.bean@gmail.com>
2020-01-09 23:09:05 -0700

    Created MovementData and Movement Scripts and PlayerMovementData Obj
    review character movement scripts and inheriting from scriptable objects

---
+++ Project1 (2020)/Assets/Scripts/DataScripts/MovementData.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class MovementData : ScriptableObject
+{
+    public float moveSpeed = 10f,
+        gravity = 3f,
+        jumpSpeed = 30f;
+
+    public int jumpCountMax = 2,
+        jumpCount = 0;
+
+    public void UpdateMoveSpeed(float amount)
+    {
+        moveSpeed += amount;
+    }
+
+    public void UpdateGravity(float amount)
+    {
+        gravity += amount;
+    }
+
+    public void UpdateJumpSpeed(float amount)
+    {
+        jumpSpeed += amount;
+    }
+
+    public void UpdateJumpCountMax(int amount)
+    {
+        jumpCountMax += amount;
+    }
+}
---
+++ Project1 (2020)/Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 position;
+
+    public MovementData PlayerMoveData;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        controller.Move(position * Time.deltaTime);
+        position.x = PlayerMoveData.moveSpeed * Input.GetAxis("Horizontal");
+
+        if (!controller.isGrounded)
+        {
+            position.y -= PlayerMoveData.gravity;
+        }
+        else
+        {
+            PlayerMoveData.jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && PlayerMoveData.jumpCount < PlayerMoveData.jumpCountMax)
+        {
+            PlayerMoveData.jumpCount++;
+            position.y = PlayerMoveData.jumpSpeed;
+        }
+    }
+
+}
