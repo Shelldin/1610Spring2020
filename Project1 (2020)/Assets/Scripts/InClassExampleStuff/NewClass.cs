6ce62be2606cedd18bbd61ab61eef8116a7358bf
Sheldon Bean <sheldon.bean@gmail.com>
2020-01-15 10:50:09 -0700

    learned about data types and syntax

---
+++ Project1 (2020)/Assets/Scripts/InClassExampleStuff/NewClass.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewClass : MonoBehaviour
+{
+    public string nameString;
+    public int intNum;
+    public float floatNum;
+    public GameObject gameObj;
+    
+    void Start()
+    {
+        print(nameString);
+    }
+}
