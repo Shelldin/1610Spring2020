5ba538a23565cfd476f27f5d48801158004e312b
Sheldon Bean <sheldon.bean@gmail.com>
2020-01-16 22:18:37 -0700

    reviewing Coroutines.  created Coroutine script

---
+++ Project1 (2020)/Assets/Scripts/Coroutines.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Coroutines : MonoBehaviour
+{
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    
+    public float seconds = 1f;
+
+    public int counter = 10;
+
+    private WaitForSeconds wfsObj;
+
+    private void Awake()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+    }
+
+    public void CallCoroutine()
+    {
+        StartCoroutine(RunCoroutine());
+    }
+
+    IEnumerator RunCoroutine()
+    {
+        startEvent.Invoke();
+        while (counter > 0)
+        {
+            repeatEvent.Invoke();
+            yield return wfsObj;
+            counter--;
+        }
+        endEvent.Invoke();
+    }
+}
