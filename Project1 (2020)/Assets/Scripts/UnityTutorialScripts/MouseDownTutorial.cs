652e93334b50c2c29248d998af3d52b14fd16bc3
Sheldon Bean <sheldon.bean@gmail.com>
2020-01-22 21:43:15 -0700

    Unity and Brackeys tutorial practice

---
+++ Project1 (2020)/Assets/Scripts/UnityTutorialScripts/MouseDownTutorial.cs
@@ -0,0 +1,20 @@
+﻿// from unity tutorial
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class MouseDownTutorial : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMouseDown()
+    {
+        rb.AddForce(transform.up *1000f);
+        rb.useGravity = true;
+    }
+
+}
