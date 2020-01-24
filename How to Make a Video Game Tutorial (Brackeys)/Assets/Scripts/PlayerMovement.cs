70854887cc0869124f41b243f4ff68141b9bfbeb
Sheldon Bean <sheldon.bean@gmail.com>
2020-01-23 23:44:26 -0700

    PlayerMovementScript in Brackeys tutorial series

---
+++ How to Make a Video Game Tutorial (Brackeys)/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿//From Brackeys Tutorials
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+
+    public float sidewaysForce = 500f;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce *Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce*Time.deltaTime, 0, 0);
+        }
+        
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce*Time.deltaTime, 0, 0);
+        }
+    }
+}
