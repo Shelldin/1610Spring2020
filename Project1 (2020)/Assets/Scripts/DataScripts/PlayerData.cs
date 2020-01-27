3b006c2794993a1a4fb994d172b74aa2fb572cdd
Sheldon Bean <sheldon.bean@gmail.com>
2020-01-26 23:58:32 -0700

    created VirtualCamera obj and VirtualCameraController Script

---
+++ Project1 (2020)/Assets/Scripts/DataScripts/PlayerData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class PlayerData : GameArtData
+{
+    public UnityAction<GameObject> instanceAction;
+    public UnityEvent onRunEvent;
+
+    public FloatData health;
+
+    public void InstancePlayer()
+    {
+        var newPlayer = Instantiate(prefab);
+        var playerSprite = newPlayer.GetComponentInChildren<SpriteRenderer>();
+        playerSprite.sprite = sprite;
+        playerSprite.color = color;
+        instanceAction(newPlayer);
+    }
+
+    public void Run()
+    {
+        onRunEvent.Invoke();
+    }
+}
---
+++ Project1 (2020)/Assets/Scripts/VirtualCameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class VirtualCameraController : MonoBehaviour
+{
+  public PlayerData playerData;
+
+  private CinemachineVirtualCamera virtualCamera;
+
+  private void Awake()
+  {
+    virtualCamera = GetComponent<CinemachineVirtualCamera>();
+    playerData.instanceAction = InstanceHandler;
+    playerData.InstancePlayer();
+  }
+
+  private void InstanceHandler(GameObject obj)
+  {
+    virtualCamera.Follow = obj.transform;
+  }
+
+}
