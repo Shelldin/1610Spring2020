2e286b211006ed7677e415b4900643fedc37f118
Sheldon Bean <sheldon.bean@gmail.com>
2020-01-28 23:35:24 -0700

    more changes to trigger stuff

--- Project1 (2020)/Assets/Scripts/DataScripts/GameArtData.cs
+++ Project1 (2020)/Assets/Scripts/DataScripts/GameArtData.cs
@@ -5,4 +5,9 @@ public class GameArtData : ScriptableObject
     public Sprite sprite;
     public Color color = Color.red;
     public GameObject prefab;
+
+    public void SetSprite(Sprite other)
+    {
+        sprite = other;
+    }
 }
