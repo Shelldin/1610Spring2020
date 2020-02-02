ab8cc0ef4040f2909daa909c19b5ff2df99fba7b
Sheldon Bean <sheldon.bean@gmail.com>
2020-02-01 23:55:55 -0700

    Collision in Brackeys Tutorial

---
+++ How to Make a Video Game Tutorial (Brackeys)/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿//From Brackeys Tutorial
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerPosition;
+    public Vector3 cameraOffset;
+
+    void Update()
+    {
+        transform.position = playerPosition.position + cameraOffset;
+    }
+}
---
+++ How to Make a Video Game Tutorial (Brackeys)/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,16 @@
+﻿//from Brackeys Tutorial
+
+using System;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    private void OnCollisionEnter(Collision colInfo)
+    {
+        if (colInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
