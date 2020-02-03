8d5540261a4af768e6d4b0eca1963d0fbfe93bf0
Sheldon Bean <sheldon.bean@gmail.com>
2020-02-03 11:21:02 -0700

    Character Controller and Movement

---
+++ Project1 (2020)/Assets/Scripts/InClassExampleStuff/CharacterMovement.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 10f,
+        gravity = -3f,
+        jumpForce = 30f;
+
+    private Vector3 positionDirection;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        positionDirection.x = Input.GetAxis("Horizontal")*speed;
+        
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            positionDirection.y = jumpForce;
+        }
+        
+        positionDirection.y += gravity;
+        controller.Move(positionDirection *Time.deltaTime);
+
+        
+        
+    }
+}
