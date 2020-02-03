81db0a5778304045124209cf1e4298cd1ba27c60
Sheldon Bean <sheldon.bean@gmail.com>
2020-02-02 22:05:27 -0700

    Brackeys tutorial on obstacles and score UI

---
+++ How to Make a Video Game Tutorial (Brackeys)/Assets/Scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿//from Brackeys Tutorial
+
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform playerTrans;
+    public Text textScore;
+
+    private float scoreCorrection;
+    
+    private void Update()
+    {
+        //My player starts at a z of -45 and I wanted to figure out how to adjust the score without shifting all the other objs in my game
+        var playerTransPosition = playerTrans.position;
+        scoreCorrection = playerTransPosition.z += 45f;
+        textScore.text = scoreCorrection.ToString("0");
+    }
+}
