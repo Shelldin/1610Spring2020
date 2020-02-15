b4f9bb0fea289650d9c7c58c7d141255918bd920
Sheldon Bean <sheldon.bean@gmail.com>
2020-02-15 13:45:49 -0700

    Created menu and ui buttons and scripts for menu

---
+++ Project1 (2020)/Assets/Scripts/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject menu;
+    
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            menu.SetActive(!menu.activeSelf);
+        }
+
+    }
+}
---
+++ Project1 (2020)/Assets/Scripts/UIButtons.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtons : MonoBehaviour
+{
+   public void RestartLevel()
+   {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+      Debug.Log("you reset the level");
+   }
+
+   public void QuitGame()
+   {
+      Application.Quit();
+      Debug.Log("you quit the game... coward.");
+   }
+}
