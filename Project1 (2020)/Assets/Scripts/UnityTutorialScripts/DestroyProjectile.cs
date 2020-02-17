b53d247f25feafa6c553ab71c96a4d77a0eceae1
Sheldon Bean <sheldon.bean@gmail.com>
2020-02-16 23:58:12 -0700

    Instatiate Unity Tutorial

---
+++ Project1 (2020)/Assets/Scripts/UnityTutorialScripts/DestroyProjectile.cs
@@ -0,0 +1,11 @@
+﻿//from unity tutorial
+using UnityEngine;
+
+public class DestroyProjectile : MonoBehaviour
+{
+    public float seconds = 5f;
+    private void Start()
+    {
+        Destroy(gameObject, seconds);
+    }
+}
---
+++ Project1 (2020)/Assets/Scripts/UnityTutorialScripts/InstantiateTutorial.cs
@@ -0,0 +1,18 @@
+﻿//from Unity Tutorial
+using UnityEngine;
+
+public class InstantiateTutorial : MonoBehaviour
+{
+    public Rigidbody proj;
+    public Transform cannonEnd;
+    public float forceFloat = 500f;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody projInstance;
+            projInstance = Instantiate(proj, cannonEnd.position, cannonEnd.rotation) as Rigidbody;
+            projInstance.AddForce(cannonEnd.forward *forceFloat);
+        }
+    }
+}
