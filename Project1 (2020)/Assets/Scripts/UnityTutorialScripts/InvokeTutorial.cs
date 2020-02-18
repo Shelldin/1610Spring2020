43d7e9068cef76d26e705e3af898f70607291f1b
Sheldon Bean <sheldon.bean@gmail.com>
2020-02-17 23:59:18 -0700

    Invoke Tutorial

---
+++ Project1 (2020)/Assets/Scripts/UnityTutorialScripts/InvokeTutorial.cs
@@ -0,0 +1,23 @@
+﻿//from unity tutorial
+
+using System;
+using UnityEngine;
+
+public class InvokeTutorial : MonoBehaviour
+{
+    public GameObject createdObj;
+    public GameObject objCreator;
+
+    private Vector3 position;
+
+    private void Start()
+    {
+        position = objCreator.transform.position;
+        Invoke("CreateObject", 3f);
+    }
+
+    private void CreateObject()
+    {
+        Instantiate(createdObj, position, Quaternion.identity);
+    }
+}
