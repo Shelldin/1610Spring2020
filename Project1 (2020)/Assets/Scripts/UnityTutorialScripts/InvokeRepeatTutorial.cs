046eb9b605bd2348dac578c3d5c17a2acf9ab075
Sheldon Bean <sheldon.bean@gmail.com>
2020-02-18 23:40:28 -0700

    finish invoke tutorial

---
+++ Project1 (2020)/Assets/Scripts/UnityTutorialScripts/InvokeRepeatTutorial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvokeRepeatTutorial : MonoBehaviour
+{
+    public GameObject spawner;
+    public GameObject spawnedObj;
+    public int spawnCount = 5;
+    public float seconds = 3f, spawnRate = 2f;
+
+    private Vector3 position;
+
+    private void Start()
+    {
+        position = spawner.transform.position;
+        InvokeRepeating("Spawn", seconds, spawnRate);
+    }
+
+    private void Update()
+    {
+        if (spawnCount <= 0)
+        {
+            CancelInvoke("Spawn");
+        }
+    }
+
+    private void Spawn()
+    {
+        Instantiate(spawnedObj, position, Quaternion.identity);
+        spawnCount--;
+    }
+}
