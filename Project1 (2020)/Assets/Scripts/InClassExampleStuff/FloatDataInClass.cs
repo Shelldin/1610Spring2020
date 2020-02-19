b77337dcc25d7131cf377b7dc8121e6f1d0219f4
Sheldon Bean <sheldon.bean@gmail.com>
2020-02-19 11:19:23 -0700

    learned scriptable objects in class

---
+++ Project1 (2020)/Assets/Scripts/InClassExampleStuff/FloatDataInClass.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+[CreateAssetMenu]
+public class FloatDataInClass : ScriptableObject
+{
+  public float value = 1f;
+
+  public void ChangeValue(float number)
+  {
+    value += number;
+  }
+}
---
+++ Project1 (2020)/Assets/Scripts/InClassExampleStuff/ImageBehaviour.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Image))]
+
+public class ImageBehaviour : MonoBehaviour
+{
+    private Image imageObj;
+    
+    void Start()
+    {
+        imageObj = GetComponent<Image>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
