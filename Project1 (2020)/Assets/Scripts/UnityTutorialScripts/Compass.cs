682690235cf190279dad625dba1bb3fbf3173d4c
Sheldon Bean <sheldon.bean@gmail.com>
2020-02-21 23:24:28 -0700

    swith and enum unity tutorial

---
+++ Project1 (2020)/Assets/Scripts/UnityTutorialScripts/Compass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//from unity tutorial
+public class Compass : MonoBehaviour
+{
+   enum CardinalDirection {N, E, S, W};
+
+   private void Start()
+   {
+      CardinalDirection direction;
+
+      direction = CardinalDirection.N;
+   }
+
+   CardinalDirection FlipDirection(CardinalDirection dir)
+   {
+      if (dir == CardinalDirection.N)
+         dir = CardinalDirection.S;
+      else if (dir == CardinalDirection.S)
+         dir = CardinalDirection.N;
+      else if (dir == CardinalDirection.E)
+         dir = CardinalDirection.W;
+      else if (dir == CardinalDirection.W)
+         dir = CardinalDirection.E;
+
+      return dir;
+   }
+}
---
+++ Project1 (2020)/Assets/Scripts/UnityTutorialScripts/ConvoScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//from unity tutorial
+public class ConvoScript : MonoBehaviour
+{
+    public int smartness = 5;
+
+    private void Greet()
+    {
+        switch (smartness)
+        {
+            case 10:
+                print("Guten Tag meine Damen und Herrn!");
+                break;
+            case 5:
+                print("how do you do fellow kids?");
+                break;
+            case 0:
+                print("I am a stegosaurus!");
+                break;
+            default:
+                print("You're too stupid for a valid message");
+                break;
+        }
+    }
+}
