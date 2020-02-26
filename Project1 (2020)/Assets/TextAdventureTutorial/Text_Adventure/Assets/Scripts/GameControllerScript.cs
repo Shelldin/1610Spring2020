9f5f802c738151a179e695f3e43b5174216b0c22
Sheldon Bean <sheldon.bean@gmail.com>
2020-02-25 23:57:18 -0700

    Created GameController Script

--- Project1 (2020)/Assets/TextAdventureTutorial/Text_Adventure/Assets/Scripts/GameControllerScript.cs
+++ Project1 (2020)/Assets/TextAdventureTutorial/Text_Adventure/Assets/Scripts/GameControllerScript.cs
@@ -1,24 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 //from unity tutorial
 [RequireComponent(typeof(RoomNav))]
 public class GameControllerScript : MonoBehaviour
 {
+    public Text displayText;
+    
     [HideInInspector] public RoomNav roomNav;
     
+    List<string> actionLog = new List<string>();
     void Awake()
     {
         roomNav = GetComponent<RoomNav>();
     }
 
+    private void Start()
+    {
+        DisplayRoomText();
+        DisplayActionLogText();
+    }
+
+    public void DisplayActionLogText()
+    {
+        string LogAsText = string.Join("\n", actionLog.ToArray());
+
+        displayText.text = LogAsText;
+    }
+
     public void DisplayRoomText()
     {
         string combinedText = roomNav.currentRoom.roomDescription + "\n";
+        
+        LogStringWithReturn(combinedText);
     }
 
-    void Update()
+    public void LogStringWithReturn(string stringToAdd)
     {
-        
+        actionLog.Add(stringToAdd + "\n");
     }
+
 }
