b406b269c874d1d099ce72f44c7f0161b3b1d662
Sheldon Bean <sheldon.bean@gmail.com>
2020-02-28 23:04:34 -0700

    Created inputtext script for unity tutorial

---
+++ Project1 (2020)/Assets/TextAdventureTutorial/Text_Adventure/Assets/Scripts/TextInput.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(GameControllerScript))]
+//from unity tutorial
+public class TextInput : MonoBehaviour
+{
+    public InputField inputField;
+    private GameControllerScript controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<GameControllerScript>();
+        inputField.onEndEdit.AddListener(AcceptStringInput);
+    }
+
+    private void AcceptStringInput(string userInput)
+  {
+      userInput = userInput.ToLower();
+      controller.LogStringWithReturn(userInput);
+      InputComplete();
+      
+  }
+
+    private void InputComplete()
+    {
+        controller.DisplayActionLogText();
+        inputField.ActivateInputField();
+        inputField.text = null;
+    }
+}
