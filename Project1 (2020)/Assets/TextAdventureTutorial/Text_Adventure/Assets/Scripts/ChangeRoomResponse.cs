c9b901b748fcf3db18185e45e823f8dfe909f501
Sheldon Bean <sheldon.bean@gmail.com>
2020-03-20 23:31:38 -0600

    Created ActionResponse Script

---
+++ Project1 (2020)/Assets/TextAdventureTutorial/Text_Adventure/Assets/Scripts/ChangeRoomResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//from Unity Tutorial 
+
+[CreateAssetMenu(menuName = "TextAdventure/ActionResponse/ChangeRoom")]
+public class ChangeRoomResponse : ActionResponse
+{
+   public RoomSO roomToChangeTo;
+
+   public override bool DoActionResponse(GameControllerScript controller)
+   {
+      if (controller.roomNav.currentRoom.roomName == requiredString)
+      {
+         controller.roomNav.currentRoom = roomToChangeTo;
+         controller.DisplayRoomText();
+         return true;
+      }
+
+      else
+      {
+         return false;
+      }
+   }
+}
