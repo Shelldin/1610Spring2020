e22d54b1a40e0810e619a8a18152e38a97f6a3e9
Sheldon Bean <sheldon.bean@gmail.com>
2020-03-25 21:08:49 -0600

    AI and NavMeshAgents

---
+++ Project1 (2020)/Assets/Scripts/InClassExampleStuff/AI/NavAgentBehaviour.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavAgentBehaviour : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    public Transform player;
+    public float speed = 8f;
+    
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.speed = speed;
+    }
+
+    void Update()
+    {
+        navMeshAgent.destination = player.position;
+    }
+}
