1964f94266c7fa686b919f2112df351c50bc4cf9
Sheldon Bean <sheldon.bean@gmail.com>
2020-03-28 20:45:28 -0600

    added armrotation script from brackeys tutorial

--- Project1 (2020)/Assets/2D Platformer Tutorial/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Project1 (2020)/Assets/2D Platformer Tutorial/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -11,22 +11,28 @@ namespace UnityStandardAssets._2D
         [SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
         [SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
 
-        private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
+        public Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
         const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
         private bool m_Grounded;            // Whether or not the player is grounded.
         private Transform m_CeilingCheck;   // A position marking where to check for ceilings
         const float k_CeilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
         private Animator m_Anim;            // Reference to the player's animator component.
         private Rigidbody2D m_Rigidbody2D;
-        private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+        private bool m_FacingRight = true; // For determining which way the player is currently facing.
+
+        public Transform playerGraphics; //reference to the graphics to flip
 
         private void Awake()
         {
+            
+            
             // Setting up references.
             m_GroundCheck = transform.Find("GroundCheck");
             m_CeilingCheck = transform.Find("CeilingCheck");
             m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
+            playerGraphics = transform.Find("Graphics");
+
         }
 
 
@@ -106,9 +112,9 @@ namespace UnityStandardAssets._2D
             m_FacingRight = !m_FacingRight;
 
             // Multiply the player's x local scale by -1.
-            Vector3 theScale = transform.localScale;
+            Vector3 theScale = playerGraphics.localScale;
             theScale.x *= -1;
-            transform.localScale = theScale;
+            playerGraphics.localScale = theScale;
         }
     }
 }
